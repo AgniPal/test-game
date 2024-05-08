ded76fcae6e95ce2faed39b335eeda36a1251e3d
AgniPal <agni.mira@gmail.com>
2024-05-08 18:20:15 +0300

    add one

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import pygame
 
 pygame.init()
 
+# Создание игрового окна
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+# Функция для создания игрового экрана
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Задаём название игрового окна
+pygame.display.set_caption("Игра тир")
+
+# Создаём иконку экрана
+icon = pygame.image.load('icon.png')
+
+
 # Создание игрового цикла
 running = True
 while running:

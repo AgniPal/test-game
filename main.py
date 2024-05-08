8e8f8125ca6a1d88c8a0f73560787f4dd6e9a669
AgniPal <agni.mira@gmail.com>
2024-05-08 18:51:46 +0300

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -12,7 +13,24 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра тир")
 
 # Создаём иконку экрана
-icon = pygame.image.load('icon.png')
+icon = pygame.image.load("img/тир.jpg")
+# Устанавливаем иконку
+pygame.display.set_icon(icon)
+
+# Создание "цели" в игре тир
+target = pygame.image.load("img/cake.png")
+
+# Задаём параметры цели
+target_width = 80
+target_height = 80
+
+# Задаём координаты для перемещения цели
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+# Цвет фона
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
 
 
 # Создание игрового цикла
